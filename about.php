fa3062370f1a5fe717cc24990e6132950cf4ed26
nurqas <149054770+nurqas@users.noreply.github.com>
2024-06-08 18:30:29 +0200

    Initial commit

---
+++ about.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Sleep Tight</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <!-- home page css -->
+  <link rel="stylesheet" type="text/css" href="css2/homepage.css" />
+  <!-- font awesome style -->
+  <link rel="stylesheet" type="text/css" href="css2/font-awesome.min.css" />
+
+  <!-- Custom styles for this template -->
+  <link href="css2/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css2/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body>
+  
+      <div class="header_bottom">
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg custom_nav-container ">
+            <a class="navbar-brand" href="index.html">
+              <span>
+                Sleep Tight
+              </span>
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                  <a class="nav-link" href="about.html"> About</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="login.php">Log In</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="signup.php">Sign Up</a>
+                </li>
+              </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <!-- end header section -->
+  </div>
+
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-md-6">
+          <div class="detail-box">
+            <h2>
+              About us
+            </h2>
+            <p>
+              Delve into the transformative power of sleep apnea monitoring. Gain insights into your sleep cycles, identify potential apnea risks, and entrust your data to our secure system.
+
+Embrace the journey of understanding your sleep patterns. Let our platform guide you through the realm of restful nights and rejuvenated mornings.
+
+Empower yourself with knowledge and peace of mind. With us, tracking sleep apnea becomes intuitive, insightful, and secure.
+            </p>
+            
+          </div>
+        </div>
+        <div class="col-lg-7 col-md-6">
+          <div class="img-box">
+            <img src="images/about-img.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end about section -->
+
+  <!-- service section -->
+
+  <section class="service_section layout_padding">
+    <div class="container ">
+      <div class="heading_container heading_center">
+        <h2> Our Services </h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-4 mx-auto">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s1.png" alt="" />
+            </div>
+            <div class="detail-box">
+              <h5>
+                Early Detection
+              </h5>
+              <p>
+                Early detection plays a pivotal role in identifying health issues before they escalate, ensuring timely intervention and better outcomes. This holds particularly true for conditions like obstructive sleep apnea (OSA), which despite being prevalent, often goes undiagnosed. Recognizing the signs of OSA early on can lead to effective management, preventing potential complications such as cardiovascular problems, daytime  fatigue, and cognitive impairment. By prioritizing early detection through screening and awareness, individuals can take proactive steps towards improved sleep quality and overall well-being.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 mx-auto">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s2.png" alt="" />
+            </div>
+            <div class="detail-box">
+              <h5>
+                Monitoring
+              </h5>
+              <p>
+                Distant monitoring is crucial tool in healthcare, especially for conditions like obstructive sleep apnea (OSA). OSA, marked by repeated airway osbtruction during sleep, demands continuous oversight to mitigate its health risks. Distant monitoring allows healthcare providers to remotely track patients' sleep patterns, respiratory events, and treatment adherence. This proactive approach facilitates early detection of worsening symptoms or treatment inefficacies, enabling timely interventions to optimize patient care. By empowering patients to actively participate in managing their condition and offering healthcare providers valuable insights, distant monitoring plays a vital role in improving outcomes and enhancing the quality of life for individuals with OSA.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 mx-auto">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s3.png" alt="" />
+            </div>
+            <div class="detail-box">
+              <h5>
+                Telemedicine
+              </h5>
+              <p>
+                You can seamlessly connect with healthcare professionals from the comfort of your own homes, enabling convenient consultations and medical support. Through secure video calls and messaging, you can receive personalized care and advice tailored to your needs, promoting accessible healthcare access anytime, anywhere
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+  </section>
+
+  <!-- info section -->
+<section class="info_section ">
+  <div class="container">
+    <h4>
+      Get In Touch
+    </h4>
+    <div class="row">
+      <div class="col-lg-10 mx-auto">
+        <div class="info_items">
+          <div class="row">
+            <div class="col-md-4">
+              <a href="">
+                <div class="item ">
+                  <div class="img-box ">
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                  </div>
+                  <p>
+                    Pfarrkirchen 
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-4">
+              <a href="">
+                <div class="item ">
+                  <div class="img-box ">
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                  </div>
+                  <p>
+                    +49 1234567890
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-4">
+              <a href="">
+                <div class="item ">
+                  <div class="img-box">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                  </div>
+                  <p>
+                    sleeptight@gmail.com
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<!-- end info_section -->
+
+
+
+</body>
+
+</html>
